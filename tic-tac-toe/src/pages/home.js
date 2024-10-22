54fee00f1903568efeea7a5e1f857320b92eb742
Viktoriia Kuznetsova <victoria.cuzneczowa23@gmail.com>
2024-10-22 18:48:30 +0300

    homescreen

--- tic-tac-toe/src/pages/home.js
+++ tic-tac-toe/src/pages/home.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import crossIcon from '../icons/cross.svg';
+import circleIcon from '../icons/circle.svg';
+import '../styles/home.css';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to Tic-Tac-Toe</h1>
-      <Link to="/game">Start Game</Link>
+    <div className="main">
+      <h1>Tic Tac Toe</h1>
+      <div className="bgCrossesAndCircles">
+            <img src={crossIcon} alt="X" className="crossMain"/>
+            <img src={circleIcon} alt="O" className="circleMain"/>
+            <img src={crossIcon} alt="X" className="crossMain"/>
+      </div>
+      <Link to="/game" className="button-link">Start Game</Link>
     </div>
   );
 };
